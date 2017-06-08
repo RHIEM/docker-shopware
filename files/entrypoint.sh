da89b7551f4884b55cbcc83e2bab7c1a1e4ef280
Marco Spittka <marco.spittka@rhiem.com>
2017-06-08 16:16:43 +0200

    added automatic build on startup

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
+
+if [ ! -e /var/www/html/shopware.php ]; then
+    echo -n "Shopware not found, installing..."
+    rm -f /var/www/html/index.html
+	git clone $SHOPWAREBUILD_GIT_REPO /var/www/html
+    echo -n "Configuring Build properties..."
+	cat > /var/www/html/build/build.properties << EOF
+db.host=$SHOPWAREBUILD_DB_HOST
+db.name=$SHOPWAREBUILD_DB_NAME
+db.user=$SHOPWAREBUILD_DB_USER
+db.password=$SHOPWAREBUILD_DB_PASSWORD
+db.port=$SHOPWAREBUILD_DB_PORT
+app.path=$SHOPWAREBUILD_APP_PATH
+app.host=$SHOPWAREBUILD_APP_HOST
+EOF
+	ant -f /var/www/html/build/build.xml  $SHOPWAREBUILD_ANT_TARGET
+    echo "done"
+fi
+
 source /etc/apache2/envvars
 exec apache2 -D FOREGROUND
