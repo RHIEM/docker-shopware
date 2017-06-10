1433f28da5db354e49c92a336ed9a0ee7f65916b
Marco Spittka <marco.spittka@rhiem.com>
2017-06-10 12:17:27 +0200

    setting environemt variables

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ ! -e /var/www/html/shopware.php ]; then
-    echo -n "Shopware not found, installing..."
+    echo -n "Shopware not found, installing...\n"
     rm -f /var/www/html/index.html
 	git clone $SHOPWAREBUILD_GIT_REPO /var/www/html
-    echo -n "Configuring Build properties..."
+    echo -n "Configuring Build properties...\n"
 	cat > /var/www/html/build/build.properties << EOF
 db.host=$SHOPWAREBUILD_DB_HOST
 db.name=$SHOPWAREBUILD_DB_NAME
@@ -14,20 +14,21 @@ db.port=$SHOPWAREBUILD_DB_PORT
 app.path=$SHOPWAREBUILD_APP_PATH
 app.host=$SHOPWAREBUILD_APP_HOST
 EOF
-	ant -f /var/www/html/build/build.xml  $SHOPWAREBUILD_ANT_TARGET
+    if [ $SHOPWAREBUILD_ANT_TARGET != "" ]; then
+	    ant -f /var/www/html/build/build.xml  $SHOPWAREBUILD_ANT_TARGET
+	fi
 	
-	if [ $SHOPWAREBUILD_DEMO_ZIP = "" ]; then
-		echo "Loading Demo-Data"
+	if [ $SHOPWAREBUILD_DEMO_ZIP != "" ]; then
+		echo "Loading Demo-Data\n"
 		curl -Lo /var/www/html/demo_data.zip http://releases.s3.shopware.com/test_images.zip #$SHOPWAREBUILD_DEMO_ZIP
 		unzip /var/www/html/demo_data.zip
 		rm /var/www/html/demo_data.zip
 	fi
-    echo "done building"
+    echo "done building\n\n"
 fi
 
-echo -n "Setting permissions..."
-	chown www-data:www-data /var/www/html/*
-done
+echo -n "Setting permissions...\n"
+	chown -R www-data:www-data /var/www/html/*
 echo "done"
 
 echo "Running apache"
